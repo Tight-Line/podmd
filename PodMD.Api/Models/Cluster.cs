eaecaad01d562fe3f6deb4fe7355b2386c6123f8
tomislavantebrncic <tomislav.brncic@demandforce.net>
2025-09-09 11:01:47 +0200

    move Host to base model

--- PodMD.Api/Models/Cluster.cs
+++ PodMD.Api/Models/Cluster.cs
@@ -2,7 +2,6 @@ namespace PodMD.Api.Models;
 
 public class Cluster : Configuration
 {
-    public required string Host { get; set; }
     public required string AccessToken { get; set; }
     public string? CaCertPem { get; set; }
 }
--- PodMD.Api/Models/Configuration.cs
+++ PodMD.Api/Models/Configuration.cs
@@ -7,6 +7,7 @@ public abstract class Configuration
     [Key] public uint Id { get; init; }
     public required Guid Guid { get; init; }
     public required string Name { get; set; }
+    public required string Host { get; set; }
     public required DateTimeOffset CreatedAt { get; init; }
     public required uint ApiKeyId { get; init; }
     public ApiKey ApiKey { get; set; }
